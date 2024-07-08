0dd43f369bcdf25c2cdd24998659d6b9e4f3c61e
Emre <emre00662299@gmail.com>
2024-07-08 14:59:34 +0300

    yedek

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: emgul <emgul@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 00:48:40 by emgul             #+#    #+#             */
-/*   Updated: 2024/07/08 13:29:26 by emgul            ###   ########.fr       */
+/*   Updated: 2024/07/08 14:58:15 by emgul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,63 +22,80 @@ char *handle_comment(char *str)
 	return (str);
 }
 
-char *handle_single_quote(char *str)
+char *handle_single_quote(char **str)
 {
 	int i;
 	char	*res;
 
 	i = 0;
-	while(str[i])
+	(*str)++;
+	while((*str)[i])
 	{
-		if (str[i] == '\'')
+		if ((*str)[i] == '\'')
 		{
-			res = ft_substr(str, 0, i);
+			res = ft_substr(*str, 0, i);
+			*str += i + 1;
 			return (res);
 		}
 		i++;
 	}
+	*str += i;
 	return (NULL);
 }
 
-char *handle_double_quote(char *str, t_env *env)
+char *handle_double_quote(char **str, t_env *env)
 {
 	int i;
 	char	*res;
 
 	i = 0;
-	while(str[i])
+	(*str)++;
+	while((*str)[i])
 	{
 		// if (str[i] == '$')
 		// 	handle_dollar_sign();
-		if (str[i] == '\"')
+		if ((*str)[i] == '\"')
 		{
-			res = ft_substr(str, 0, i);
+			res = ft_substr(*str, 0, i);
+			*str += i + 1;
 			return (res);
 		}
 		i++;
 	}
+	*str += i;
 	return (NULL);
 }
 
-char *handle_space(char *str, t_env *env)
+char *handle_space(char **str, t_env *env)
 {
 	int 	i;
 	char	*res;
 
 	i = 0;
-	while(str[i] == ' ')
+	while((*str)[i] == ' ' || ((*str)[i] >= 8 && (*str)[i] <= 13) || ((*str)[i] == '\\'))
 		i++;
-	while(str[i])
+	while((*str)[i])
 	{
+		if (((*str)[i] == '\'' || (*str)[i] == '\"') 
+		&& ((*str)[i - 1] == ' ' || (*str)[i - 1] == '\n' || (*str)[i - 1] == '\t'))
+			return (NULL);
 		// if (str[i] == '$')
 		// 	handle_dollar_sign();
-		if (str[i] == ' ')
+		if ((*str)[i] == ' ' || ((*str)[i] >= 8 && (*str)[i] <= 13) || ((*str)[i] == '\\'))
 		{
-			res = ft_substr(str, 0, i);
+			res = ft_substr(*str, 0, i);
+			*str += i;
 			return (res);
 		}
 		i++;
 	}
+	if ((*str)[i] == '\0')
+	{
+		res = ft_substr(*str, 0, i);
+		*str += i;
+		return (res);
+	}
+	*str += i;
 	return (NULL);
 }
 
@@ -115,55 +132,38 @@ void	lstadd_back_token(t_tokens **lst, t_tokens *new)
 	return ;
 }
 
-int skip_spaces(char *input, int *i)
-{
-	while (*input == ' ')
-	{
-		*i++;
-		input++;
-	}
-}
+
 
 t_tokens *tokenizer(char *input, t_env *env)
 {
-	int i;
 	char *res;
 	t_tokens *tokens;
 
-	i = 0;
-	while (input[i])
+	while (*input)
 	{
 		res = NULL;
-		skip_spaces((input + i), &i);
-		if (input[i] == '\'')
-		{
-			res = handle_single_quote(&input[i + 1]);
-			i += 2;
-		}
-		skip_spaces((input + i), &i);
-		if (!res && input[i] == '\"')
-		{
-			res = handle_double_quote(&input[i + 1], env);
-			i += 2;
-		}
-		if (!res && (input[i] == ' ' || i == 0))
-			res = handle_space(&input[i], env);
+		if (*input == '\'')
+			res = handle_single_quote(&input);
+		if (!res && *input == '\"')
+			res = handle_double_quote(&input, env);
+		if (!res)
+			res = handle_space(&input, env);
 		if (res)
 		{
-			i += ft_strlen(res);
-			lstadd_back_token(&tokens, new_token(res));
+			lstadd_back_token(&tokens, new_token(ft_strtrim(res, "\n\t ")));
+			free(res);
 		}
-		else
-			i++;
+		if (!res)
+			input++;
 	}
-	printf("SONUC: %s ve İ: %i\n", tokens->token, i);
+	return (tokens);
 }
 
 
 int main(int ac, char **av, char **env)
 {
 	av[0] = "./minishell";
-	av[1] = "mısra kahve 'içiyor' 've' !@=2emre       'yeşil çay' içiyor!!";
+	av[1] = " poğaça       \n	\t		  börek emre'mısra''yeşil çay' içiyor!!";
 	av[2] = NULL;
 	t_env *environment;
 	// environment = get_env(env);
