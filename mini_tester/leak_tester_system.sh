50aa6d1f3684b69fc79425ce30f862cdd4cb1f88
Emre Gül <emregl246@outlook.com.tr>
2024-09-26 22:52:52 +0300

    Project directories have been organized.

--- mini_tester/leak_tester_system.sh
+++ mini_tester/leak_tester_system.sh
@@ -1,17 +1,12 @@
-
 #!/bin/bash
 
-# Komutların bulunduğu dosya
 input_file="commands.txt"
-
-# Çıkış dosyası
 output_file="output.txt"
 
-# Eğer çıkış dosyası varsa sıfırlayalım
 > "$output_file"
 > "$clean_file"
 make re
-# Komutları sırayla oku ve çalıştır
+
 while IFS= read -r cmd; do
     echo "Running: valgrind <<< \"$cmd\""
     ./minishell <<< "$cmd" >> "$output_file"
@@ -20,4 +15,3 @@ while IFS= read -r cmd; do
 done < "$input_file"
 
 echo "İşlemleri tamamlandı. Çıkış dosyasına yazıldı: $output_file"
-
--- mini_tester/leak_tester_valgrind.sh
+++ mini_tester/leak_tester_valgrind.sh
@@ -1,21 +1,14 @@
-
 #!/bin/bash
 
-# Komutların bulunduğu dosya
 input_file="commands.txt"
-
-# Çıkış dosyası
 output_file="valgrind_output.txt"
 clean_file="out.txt"
-# Valgrind ile çalıştırılacak program
 
-# Eğer çıkış dosyası varsa sıfırlayalım
 > "$output_file"
 > "$clean_file"
 make re
-# Komutları sırayla oku ve çalıştır
+
 while IFS= read -r cmd; do
-    # Valgrind ile programı çalıştır ve sonuçları output_file'a ekle
     echo "Running: valgrind <<< \"$cmd\""
     make leak <<< "$cmd" >> "$output_file" 2>&1
     echo -e "\n---\n" >> "$output_file"
